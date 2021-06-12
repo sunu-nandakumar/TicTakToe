99bf98611f8f1f0cd201b971b3eacd380f67b2bb
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-12 11:29:36 +0530

    refactoring the code

--- TicTakToeGame.java
+++ TicTakToeGame.java
@@ -37,7 +37,12 @@ public class TicTakToeGame {
 
 	public static void showBoard(char[] board) {
 		
-		System.out.println(board);
+		System.out.println(board[1] +  " | " +board[2] + " | " +board[3] );
+		System.out.println("---------- ");
+		System.out.println(board[4] +  " | " +board[5] + " | " +board[6] );
+		System.out.println("---------- ");
+		System.out.println(board[7] +  " | " +board[8] + " | " +board[9] );
+		
 	
 
 	}
