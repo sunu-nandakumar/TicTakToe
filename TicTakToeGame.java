b03c57ce56eef15692c970efb600478b0725970a
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-12 10:21:15 +0530

    adding the second user case

--- TicTakToeGame.java
+++ TicTakToeGame.java
@@ -1,23 +1,51 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTakToeGame {
+public static char player = ' ';
+public  static char computer = ' ';
+	char[] board = new char[10];
 
-	
-char [] board = new char [10];
+	public void createBoard() {
+		for (int i = 1; i < board.length; i++) {
 
-public  void createBoard() {
-	for(int i =1; i<board.length;i++) {
-		
-		board[i] = ' ';
+			board[i] = ' ';
+		}
 	}
-}
-	
-	public static void main(String[] args) {
+
+	public  void chooseinput() {
 		
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println(" Enter the input you want");
+		char input = scan.next().charAt(0);
+
+		if (input == 'x' || input == 'X' ) {
+			System.out.println(" you have choosen x as your input ");
+			player = input;
+			computer = 'O';
+
+		}
+
+		else if (input == 'o' || input == 'O') {
+			System.out.println(" you have choosen o as your input as " );
+			player = input;
+			computer = 'O';
+
+		}
+		else {
+			System.out.println(" please enter a valid input ");
+		}
+
+	}
+
+	public static void main(String[] args) {
+
 		TicTakToeGame obj = new TicTakToeGame();
 		obj.createBoard();
-		System.out.println(" adding the board");
-	
+		obj.chooseinput();
+
 	}
 
 }
