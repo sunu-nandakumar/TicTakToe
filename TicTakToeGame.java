6267c56ee59d09e12189bf148d510283c09c67c1
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-12 12:12:34 +0530

    adding the uc4

--- TicTakToeGame.java
+++ TicTakToeGame.java
@@ -3,25 +3,27 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTakToeGame {
-	public static char player = ' ';
-	public static char computer = ' ';
 	
 
 	public static void main(String[] args) {
 
 		TicTakToeGame obj = new TicTakToeGame();
-		char [] board = TicTakToeGame.createBoard();
+		char[] board = TicTakToeGame.createBoard();
 		Scanner scan = new Scanner(System.in);
 		char player = chooseInput(scan);
 		char computer = (player == 'X') ? 'O' : 'X';
 		System.out.println(player + " is your choosen value ");
-		showBoard(board);
+		System.out.println(" enter the position you want place the input");
+		
+		placePeice(board , player );
+		
+
 
 	}
 
 	public static char[] createBoard() {
 		char[] board = new char[10];
-		
+
 		for (int i = 1; i < board.length; i++) {
 
 			board[i] = ' ';
@@ -36,14 +38,27 @@ public class TicTakToeGame {
 	}
 
 	public static void showBoard(char[] board) {
-		
-		System.out.println(board[1] +  " | " +board[2] + " | " +board[3] );
-		System.out.println("---------- ");
-		System.out.println(board[4] +  " | " +board[5] + " | " +board[6] );
-		System.out.println("---------- ");
-		System.out.println(board[7] +  " | " +board[8] + " | " +board[9] );
-		
-	
 
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("---------- ");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("---------- ");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+
+	}
+
+	public static void placePeice(char[] board , char player) {
+		Scanner pos = new Scanner(System.in);
+		int position = pos.nextInt();
+		
+		if (board[position] == ' ') {
+			board[position] = player;
+			showBoard(board);
+			
+			}
+		else {
+			System.out.println(" enter another position ");
+		}
+		
 	}
 }
