2001fa4f86cb5ff3a476b09db6d1e06df7bf0264
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-12 10:30:29 +0530

    refactoring/formatting the code

--- TicTakToeGame.java
+++ TicTakToeGame.java
@@ -3,25 +3,33 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTakToeGame {
-public static char player = ' ';
-public  static char computer = ' ';
-	char[] board = new char[10];
+	public static char player = ' ';
+	public static char computer = ' ';
+	
+
+	public static void main(String[] args) {
+
+		TicTakToeGame obj = new TicTakToeGame();
+		obj.createBoard();
+		obj.chooseinput();
+
+	}
 
 	public void createBoard() {
+		char[] board = new char[10];
 		for (int i = 1; i < board.length; i++) {
 
 			board[i] = ' ';
 		}
 	}
 
-	public  void chooseinput() {
-		
+	public void chooseinput() {
 
 		Scanner scan = new Scanner(System.in);
 		System.out.println(" Enter the input you want");
 		char input = scan.next().charAt(0);
 
-		if (input == 'x' || input == 'X' ) {
+		if (input == 'x' || input == 'X') {
 			System.out.println(" you have choosen x as your input ");
 			player = input;
 			computer = 'O';
@@ -29,22 +37,13 @@ public  static char computer = ' ';
 		}
 
 		else if (input == 'o' || input == 'O') {
-			System.out.println(" you have choosen o as your input as " );
+			System.out.println(" you have choosen o as your input as ");
 			player = input;
 			computer = 'O';
 
-		}
-		else {
+		} else {
 			System.out.println(" please enter a valid input ");
 		}
-
-	}
-
-	public static void main(String[] args) {
-
-		TicTakToeGame obj = new TicTakToeGame();
-		obj.createBoard();
-		obj.chooseinput();
 
 	}
 
